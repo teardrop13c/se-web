e9b9bb916bcdae125a65eef6c386300d9b4cfa25
teardrop <natthabhat.c@ku.th>
2024-02-15 20:39:16 +0700

    UserNews

---
+++ src/components/User/UserPage/ContentPage/UserNewsContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function UserNewsContent() {
+  return (
+    <>
+      <div className="rounded-rectangle">
+        <p className="faculty-text">ข่าวสาร</p>
+        <div className="file-upload">
+          <label htmlFor="fileInput">สร้างข่าว</label>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default UserNewsContent
--- src/components/User/UserPage/UserNews.jsx
+++ src/components/User/UserPage/UserNews.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Navbar from '../../Navbar'
 import UserMenu from '../UserMenu/UserMenu'
+import UserNewsContent from './ContentPage/UserNewsContent'
 
 function UserNews() {
   return (
     <section id="main-layout">
       <Navbar />
       <UserMenu />
-      <div className="content">
-        <h2>News Page</h2>
-      </div>
+      <UserNewsContent />
     </section>
   )
 }
