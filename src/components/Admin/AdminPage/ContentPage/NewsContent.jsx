15630635c5b4a54681477a6729d718056e565ec1
Natthabhat Chantharakran <natthabhat.ch@gmail.com>
2024-02-21 01:33:18 +0700

    Edit News Page (ADMIN)
    Modal News

--- src/components/Admin/AdminPage/ContentPage/NewsContent.jsx
+++ src/components/Admin/AdminPage/ContentPage/NewsContent.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import "./NewsContent.css"
+import { useState } from 'react';
 
 function NewsContent() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  const Modal = ({ children }) => {
+    return (
+      <div className="modal">
+        <div className="modal-overlay"></div>
+        {children}
+      </div>
+    );
+  };
   return (
     <>
       <div className="rounded-rectangle">
         <p className="faculty-text">ข่าวสาร</p>
-        <div className="file-upload">
-          <label htmlFor="fileInput">สร้างข่าว</label>
-        </div>
+        <button  className="create-news-button" onClick={handleOpenModal}>สร้างข่าว</button>
+        {isModalOpen && (
+        <Modal>
+          <div className="modal-content">
+            <h2>กรุณาใส่หัวข้อข่าว</h2>
+            <input type="text" placeholder="หัวข้อข่าว" />
+            <textarea placeholder="เนื้อหาข่าว"></textarea>
+            <div className="modal-actions">
+              <button onClick={handleCloseModal}>ปิด</button>
+              <button>โพสต์</button>
+            </div>
+          </div>
+        </Modal>
+      )}
       </div>
     </>
   )
